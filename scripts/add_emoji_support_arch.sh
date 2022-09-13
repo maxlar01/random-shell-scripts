3a4a9e579b943d6d300526c92c6edcdb6abbb6e2
Moustafa Hassan <72808111+maxlar01@users.noreply.github.com>
2022-09-13 16:40:06 +0200

    Update add_emoji_support_arch.sh

--- scripts/add_emoji_support_arch.sh
+++ scripts/add_emoji_support_arch.sh
@@ -4,8 +4,6 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 echo "Setting up Noto Emoji font..."
 # 1 - install  noto-fonts-emoji package
 pacman -S noto-fonts-emoji --needed
-# pacman -S powerline-fonts --needed
-echo "Recommended system font: inconsolata regular (ttf-inconsolata or powerline-fonts)"
 # 2 - add font config to /etc/fonts/conf.d/01-notosans.conf
 echo "<?xml version=\"1.0\"?>
 <!DOCTYPE fontconfig SYSTEM \"fonts.dtd\">
@@ -45,4 +43,3 @@ echo "<?xml version=\"1.0\"?>
 # 3 - update font cache via fc-cache
 fc-cache
 echo "Noto Emoji Font installed! You may need to restart applications like chrome. If chrome displays no symbols or no letters, your default font contains emojis."
-echo "consider inconsolata regular"
